24be79fc87c24ff5c463150dce9ce8e6cab2a185
DarklingFox <Nathanjack0121@gmail.com>
2025-02-25 11:50:55 +0000

    25.2.25

--- Graded Unite Game/Assets/Scripts/MoveThePlayer(UnusedCode)).cs
+++ Graded Unite Game/Assets/Scripts/MoveThePlayer(UnusedCode)).cs
@@ -2,48 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveThePlayer : MonoBehaviour
-{
+//public class MoveThePlayer : MonoBehaviour
+//{
     //add veribles
-    public string left;
-    public string right;
-    public string Jump;
-    public Rigidbody2D Rigidbody;
+   // public string left;
+   // public string right;
+    //public string Jump;
+    //public Rigidbody2D Rigidbody;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Rigidbody = this.GetComponent<Rigidbody2D>();
-    }
+   // void Start()
+    //{
+     //   Rigidbody2D = this.GetComponent<Rigidbody2D>();
+    //}
 
     // Update is called once per frame
     // move player
-    public void Update(float deltaTime, Vector3 move)
-    {
+   // public void Update(float deltaTime, Vector3 move)
+   // {
         //I know some of code just rotates sprite, just trying to get it to do something first
-        if (Input.GetKey(Jump))
-        {
-            this.transform.Rotate (new Vector3(0f, 5f, 0f) * deltaTime * 1);
-        }
+     //   if (Input.GetKey(Jump))
+     //   {
+      //      this.transform.Rotate (new Vector3(0f, 5f, 0f) * deltaTime * 1);
+      //  }
 
-        if (Input.GetKey(left))
-        {
+      //  if (Input.GetKey(left))
+       // {
             //this.transform.Rotate(new Vector3(0f, 0f, 50f) * deltaTime);
-            move = new Vector3(0f, 0.1f, 0f);
-        }
+       //     move = new Vector3(0f, 0.1f, 0f);
+       // }
 
-        if (Input.GetKey(right))
-        {
-            this.transform.Rotate(new Vector3(0f, 0f, -50f) * deltaTime);
-        }
-        this.transform.Translate(move);
-    }
+      //  if (Input.GetKey(right))
+      //  {
+       //     this.transform.Rotate(new Vector3(0f, 0f, -50f) * deltaTime);
+       // }
+       // this.transform.Translate(move);
+   // }
 
-    private void FixedUpdate()
-    {
-        if (Input.GetKey(Jump))
-        {
-           Rigidbody.AddForce(this.transform.up * 5);
-        }
-    }
-}
+    //private void FixedUpdate()
+    //{
+       // if (Input.GetKey(Jump))
+       // {
+      //     Rigidbody.AddForce(this.transform.up * 5);
+      //  }
+   // }
+//}
---
+++ Graded Unite Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //add veribles
+    public string left = "a";
+    public string right = "d";
+    public string Jump = "w";
+    public Rigidbody2D Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    // move player
+    public void Update()
+    {
+        Vector3 move = new Vector3(0, 0);
+        //I know some of code just rotates sprite, just trying to get it to do something first
+        if (Input.GetKey(Jump))
+        {
+            this.transform.Rotate(new Vector3(0f, 5f, 0f) * Time.deltaTime * 1);
+        }
+
+        if (Input.GetKey(left))
+        {
+            this.transform.Rotate(new Vector3(0f, 0f, 50f) * Time.deltaTime);
+            //move = new Vector3(0f, 0.1f, 0f);
+        }
+
+        if (Input.GetKey(right))
+        {
+            this.transform.Rotate(new Vector3(0f, 0f, -50f) * Time.deltaTime);
+        }
+        //this.transform.Translate(move);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(Jump))
+        {
+            Rigidbody.AddForce(this.transform.up * 5);
+        }
+    }
+}
