34b75317338d6642197366be902e6ae2039db696
DarklingFox <Nathanjack0121@gmail.com>
2025-02-27 11:57:05 +0000

    27.2.25
    started to code Rocket

---
+++ Graded Unite Game/Assets/Scripts/FireBullet.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+
+    //Add Veriables 
+    public string fire;
+    public float lastFired;
+    public GameObject Rocket;
+    Rigidbody2D Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set component
+        Rigidbody = this.GetComponent<Rigidbody2D>();
+
+        //initalise variables
+        lastFired = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //add veriable
+        GameObject firedBullet;
+
+        //get Rocket to fire
+        if (Input.GetKey (fire))
+        {
+            if (Time.time > lastFired + 1)
+            {
+                firedBullet = Instantiate(Rocket, this.transform.position + (this.transform.up * 10), this.transform.rotation);
+                lastFired = Time.time;
+            }
+        }
+    }
+}
