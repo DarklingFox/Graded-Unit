e8bfb60cb26360fac5c8e06ad8be9fd87cbdba4e
DarklingFox <Nathanjack0121@gmail.com>
2025-03-08 14:55:32 +0000

    8.3.25

--- Graded Unite Game/Assets/Scripts/HealthManager.cs
+++ Graded Unite Game/Assets/Scripts/HealthManager.cs
@@ -13,6 +13,9 @@ public class HealthManager : MonoBehaviour
     void Start()
     {
         CurrentHealth = MaxHealth;
+
+        //Call Method
+        DrawHeath();
     }
 
     // Update is called once per frame
@@ -21,13 +24,44 @@ public class HealthManager : MonoBehaviour
         
     }
 
+    //Display Health
+    public void DrawHeath()
+    {
+        float MaxHealthRemainder = MaxHealth % 2;
+        int HealthCreated = (int)((MaxHealth / 2) + MaxHealthRemainder);
+
+        createHealth();
+
+       // for (int i = 0; i < Healthbar.Count; i++)
+        {
+            //int HealtheRemainder = (int)Mathf.Clamp(CurrentHealth - (i * 2), 0, 10);
+        }
+    }
+
+    // Create Health bar for Player and Enemy
     public void createHealth()
     {
         GameObject newHealthbar = Instantiate(HealthbarPrefab);
         newHealthbar.transform.SetParent(transform);
 
         Health HealthComponent = newHealthbar.GetComponent<Health>();
-        HealthComponent.SetSprite(Healthleft.Health10);
+        HealthComponent.SetSprite(Health.CurrentHealthStage.Health10);
         HealthList.Add(HealthComponent);
+    }
+
+    public void DealDamage(float Amount)
+    {
+        CurrentHealth = Amount;
+
+    }
+
+//When Collides with Rocket
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Rocket")
+        {
+            //collision.gameObject.GetComponent.DealDamage(1);
+        }
+
     }
 }
